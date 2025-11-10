4d1cfed0a2ad1c1e7a71b9cad00e5de6c3ea3ccf
Andrei <iulian.andrei.manea@gmail.com>
2025-11-10 18:47:59 +0200

    B

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var04/PracticalTest01Var04MainActivity.kt
@@ -1,20 +1,64 @@
 package ro.pub.cs.systems.eim.practicaltest01var04
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.CheckBox
+import android.widget.Toast
 
 class PracticalTest01Var04MainActivity : AppCompatActivity() {
+    private lateinit var input1: EditText
+    private lateinit var input2: EditText
+    private lateinit var uneditable: TextView
+
+    private lateinit var bottomText: String
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_practical_test01_var04_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        input1 = findViewById(R.id.input1)
+        input2 = findViewById(R.id.input2)
+        uneditable = findViewById(R.id.uneditable)
+        bottomText = ""
+
+        val displayButton = findViewById<Button>(R.id.set_button)
+        displayButton.setOnClickListener {
+            val input1Text = input1.text.toString()
+            val input2Text = input2.text.toString()
+
+            val checkbox1 = findViewById<CheckBox>(R.id.checkbox1)
+            val checkbox2 = findViewById<CheckBox>(R.id.checkbox2)
+
+            if (checkbox1.isChecked && input1Text.isEmpty()) {
+                Toast.makeText(this, "Camp necompletat", Toast.LENGTH_LONG).show()
+            } else if (checkbox1.isChecked) {
+                bottomText += "$input1Text\n"
+            }
+
+            if (checkbox2.isChecked && input2Text.isEmpty()) {
+                Toast.makeText(this, "Camp necompletat", Toast.LENGTH_LONG).show()
+            } else if (checkbox2.isChecked) {
+                bottomText += "$input2Text\n"
+            }
+
+            uneditable.text = bottomText
         }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("input1", input1.text.toString())
+        outState.putString("input2", input2.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        input1.setText(savedInstanceState.getString("input1"))
+        input2.setText(savedInstanceState.getString("input2"))
     }
 }
